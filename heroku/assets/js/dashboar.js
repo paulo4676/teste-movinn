23eceee88df3ffc8fb434fca4d1878d431930bb7
Paulo Henrique de Souza Lima <Paulo.henriqueslima.46@gmail.com>
2022-10-06 17:25:35 +0000

    teste

--- heroku/assets/js/dashboar.js
+++ heroku/assets/js/dashboar.js
@@ -3,3 +3,9 @@ $(window).on('click', function(event){
     console.log("jquery ok");
     console.log(event.target.id);
 });
+
+$(document).ready(function(){
+    $('pages').click(function(event){
+        alert(event.target.id);
+    });
+});
