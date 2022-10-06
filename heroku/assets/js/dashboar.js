158719965064ce649ad687411b520245369a7191
Paulo Henrique de Souza Lima <Paulo.henriqueslima.46@gmail.com>
2022-10-06 16:04:52 +0000

    teste jquery

--- heroku/assets/js/dashboar.js
+++ heroku/assets/js/dashboar.js
@@ -1,3 +1,6 @@
-function changeHome() {
-    alert("teste");
-}
+
+$("#Home").click(function(){
+    var conteudo = $("#divPrincipal").text();
+    alert(conteudo);
+    $("#divPrincipal").text("<h3>Novo conteudo</h3>")
+});
