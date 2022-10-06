5fef8b6248d04812b8a9b2ecaef0c606662c81f4
Paulo Henrique de Souza Lima <Paulo.henriqueslima.46@gmail.com>
2022-10-06 16:56:48 +0000

    teste js

--- heroku/assets/js/dashboar.js
+++ heroku/assets/js/dashboar.js
@@ -3,3 +3,8 @@ $(document).on("click",".appDetails", function () {
     var clickedBtnID = $(this).attr('id');
     console.log(clickedBtnID)
 });
+
+
+$(document).on("click",".appDetails", function (event) {
+    alert(event.target.id);
+});
